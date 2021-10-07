a039c6f33f4c7a78cff6617ab35554494ab44a3a
John Jannetty <jjannetty@gmail.com>
2021-10-07 11:51:16 -0400

    add albums to website

---
+++ scripts.js
@@ -0,0 +1,32 @@
+window.methodName = () => {
+  let clients = document.getElementsByClassName('portfolio-list-link');
+  let albums = document.getElementsByClassName('album');
+  let overlay = document.querySelector('.overlay');
+
+  overlay.addEventListener('click', () => {
+    let displayed = document.querySelector('.album.display');
+
+    overlay.classList.remove('display-flex')
+    displayed.classList.remove('display')
+  })
+
+  Array.from(clients).forEach((client, index) => { 
+    let album = albums.item(index)
+
+    client.addEventListener('mouseenter', () => {
+      let windowWidth = window.innerWidth
+      let displayed = document.querySelector('.album.display');
+
+      if (windowWidth > 414) {
+        if (!displayed) {
+          overlay.classList.add('display-flex')
+        }
+
+        if (!album.classList.contains('display')) {
+          album.classList.add('display')
+          displayed.classList.remove('display')
+        }
+      }
+    })
+  } )
+}
