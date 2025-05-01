4bf335e3acb4be9bcdbd2ac68b361418cd783431
John Jannetty <jjannetty@gmail.com>
2025-05-01 09:22:42 -0400

    new site

--- portfolio.js
+++ portfolio.js
@@ -1,137 +1,217 @@
 export const portfolio = [
   {
-    name: "Reactive",
-    site: "https://www.reactive.live/",
-    album: "images/albums/album-reactive.png"
+    name: "AI Growth Labs",
+    site: "https://aigrowthlabs.com/",
+    album: "images/albums/album-ai-growth-labs.png",
+    description: "AI-driven business scaling solutions",
+    liquidEvent: false
   },
   {
-    name: "Postscript",
-    site: "https://www.postscript.io/",
-    album: "images/albums/album-postscript.png"
-  },
-  // {
-  //   name: "Treat",
-  //   site: "https://treat.xyz/",
-  //   album: "images/albums/album-treat.png"
-  // },
-  {
-    name: "Typeface",
-    site: "https://www.typeface.ai/",
-    album: "images/albums/album-typeface.png"
-  },
-  {
-    name: "Gordian",
-    site: "https://www.gordiansoftware.com/",
-    album: "images/albums/album-gordian.png"
-  },
-  {
-    name: "Skipify",
-    site: "https://www.skipify.com/",
-    album: "images/albums/album-skipify.png"
-  },
-  {
-    name: "Intelligems",
-    site: "https://www.intelligems.io/",
-    album: "images/albums/album-intelligems.png"
-  },
-  {
-    name: "Freshbooks",
-    site: "https://www.freshbooks.com/",
-    album: "images/albums/album-freshbooks.png"
-  },
-  {
-    name: "Peel",
-    site: "https://www.peelinsights.com/",
-    album: "images/albums/album-peel.png"
-  },
-  {
-    name: "Koverly",
-    site: "https://www.koverly.com/",
-    album: "images/albums/album-koverly.png"
-  },
-  {
-    name: "Klaviyo",
-    site: "https://www.klaviyo.com/",
-    album: "images/albums/album-klaviyo.png"
-  },
-  {
-    name: "Repeat",
-    site: "https://www.getrepeat.io/",
-    album: "images/albums/album-repeat.png"
-  },
-  {
-    name: "Xeno",
-    site: "https://getxeno.com/",
-    album: "images/albums/album-xeno.png"
+    name: "Affiliate",
+    site: "https://www.affiliate.com/",
+    album: "images/albums/album-affiliate.png",
+    description: "Marketing distribution system",
+    liquidEvent: false
   },
   {
     name: "Blue Onion",
     site: "https://www.blueonionlabs.com/",
-    album: "images/albums/album-blue-onion.png"
+    album: "images/albums/album-blue-onion.png",
+    description: "Automated financial ledger",
+    liquidEvent: false
   },
   {
     name: "Chooose",
     site: "https://chooose.today/",
-    album: "images/albums/album-chooose.png"
+    album: "images/albums/album-chooose.png",
+    description: "Sustainable fuels management",
+    liquidEvent: false
   },
   {
     name: "Drivepoint",
     site: "https://www.drivepoint.io/",
-    album: "images/albums/album-drivepoint.png"
-  },
-  {
-    name: "Affiliate.com",
-    site: "https://www.affiliate.com/",
-    album: "images/albums/album-affiliate.png"
-  },
-  {
-    name: "Popchew",
-    site: "https://popchew.com/",
-    album: "images/albums/album-popchew.png"
-  },
-  {
-    name: "Two Boxes",
-    site: "https://www.twoboxes.com/",
-    album: "images/albums/album-two-boxes.png"
-  },
-  {
-    name: "Rain",
-    site: "https://www.raincards.xyz/",
-    album: "images/albums/album-rain.png"
-  },
-  {
-    name: "Paylode",
-    site: "https://www.paylode.com/",
-    album: "images/albums/album-paylode.png"
-  },
-  {
-    name: "Starday",
-    site: "https://www.stardayfoods.com/",
-    album: "images/albums/album-starday.png"
+    album: "images/albums/album-drivepoint.png",
+    description: "Strategic finance platform",
+    liquidEvent: false
   },
   {
     name: "Factored Quality",
     site: "https://www.factoredquality.com/",
-    album: "images/albums/album-factored-quality.png"
+    album: "images/albums/album-factored-quality.png",
+    description: "International quality control",
+    liquidEvent: true,
+    eventText: "Acquired by Pietra"
   },
   {
-    name: "Taxwire",
-    site: "https://www.taxwire.co/",
-    album: "images/albums/album-taxwire.png"
+    name: "FAM",
+    site: "#",
+    album: "images/albums/album-placeholder.png",
+    description: "Family travel booking",
+    liquidEvent: false
+  },
+  {
+    name: "Faro",
+    site: "#",
+    album: "images/albums/album-placeholder.png",
+    description: "Global apparel upcycling",
+    liquidEvent: false
+  },
+  {
+    name: "Freshbooks",
+    site: "https://www.freshbooks.com/",
+    album: "images/albums/album-freshbooks.png",
+    description: "Accounting for SMB",
+    liquidEvent: false
+  },
+  {
+    name: "Gordian",
+    site: "https://www.gordiansoftware.com/",
+    album: "images/albums/album-gordian.png",
+    description: "Airline ancillary API",
+    liquidEvent: false
+  },
+  {
+    name: "Intelligems",
+    site: "https://www.intelligems.io/",
+    album: "images/albums/album-intelligems.png",
+    description: "Profit optimization tool",
+    liquidEvent: false
+  },
+  {
+    name: "Klaviyo",
+    site: "https://www.klaviyo.com/",
+    album: "images/albums/album-klaviyo.png",
+    description: "Sales & marketing automation",
+    liquidEvent: true,
+    eventText: "NYSE: KVYO"
+  },
+  {
+    name: "Koverly",
+    site: "https://www.koverly.com/",
+    album: "images/albums/album-koverly.png",
+    description: "Cross border lending",
+    liquidEvent: false
   },
   {
     name: "LightLabs",
     site: "https://www.lightlabs.com/",
-    album: "images/albums/album-light-labs.png"
+    album: "images/albums/album-lightlabs.png",
+    description: "CPG ingredient testing",
+    liquidEvent: false
   },
   {
-    name: "Stealth",
-    site: "https://www.vinyl.vc",
-    album: "images/albums/album-stealth.png"
+    name: "Paylode",
+    site: "https://www.paylode.com/",
+    album: "images/albums/album-paylode.png",
+    description: "Loyalty program management",
+    liquidEvent: false
   },
   {
-    name: "AI Growth Labs",
-    site: "https://aigrowthlabs.com/",
-    album: "images/albums/album-ai-growth-labs.png"
+    name: "Peel",
+    site: "https://www.peelinsights.com/",
+    album: "images/albums/album-peel.png",
+    description: "Automated ecomm analytics",
+    liquidEvent: true,
+    eventText: "Acquired by Shopify"
+  },
+  {
+    name: "Popchew",
+    site: "https://popchew.com/",
+    album: "images/albums/album-popchew.png",
+    description: "Omnichannel GenZ Restaurant",
+    liquidEvent: false
+  },
+  {
+    name: "Postscript",
+    site: "https://www.postscript.io/",
+    album: "images/albums/album-postscript.png",
+    description: "SMS marketing automation",
+    liquidEvent: false
+  },
+  {
+    name: "Rain",
+    site: "https://www.raincards.xyz/",
+    album: "images/albums/album-rain.png",
+    description: "Digital money rails",
+    liquidEvent: false
+  },
+  {
+    name: "Reactive",
+    site: "https://www.reactive.live/",
+    album: "images/albums/album-reactive.png",
+    description: "Live shopping infrastructure",
+    liquidEvent: false
+  },
+  {
+    name: "Repeat",
+    site: "https://www.getrepeat.io/",
+    album: "images/albums/album-repeat.png",
+    description: "Digital subscription management",
+    liquidEvent: true,
+    eventText: "Acquired by TINY"
+  },
+  {
+    name: "Sequen",
+    site: "https://www.sequen.ai/",
+    album: "images/albums/album-placeholder.png",
+    description: "Search and personalization",
+    liquidEvent: false
+  },
+  {
+    name: "Skipify",
+    site: "https://www.skipify.com/",
+    album: "images/albums/album-skipify.png",
+    description: "One click payments",
+    liquidEvent: true,
+    eventText: "Acquired by Shopify"
+  },
+  {
+    name: "Starday",
+    site: "https://www.stardayfoods.com/",
+    album: "images/albums/album-starday.png",
+    description: "Sabermetrics for retail",
+    liquidEvent: false
+  },
+  {
+    name: "Taxwire",
+    site: "https://www.taxwire.co/",
+    album: "images/albums/album-taxwire.png",
+    description: "Tax automation software",
+    liquidEvent: false
+  },
+  {
+    name: "TravelAgent",
+    site: "#",
+    album: "images/albums/album-placeholder.png",
+    description: "Modern travel booking",
+    liquidEvent: false
+  },
+  {
+    name: "Treat",
+    site: "#",
+    album: "images/albums/album-placeholder.png",
+    description: "Marketing collateral generation",
+    liquidEvent: false
+  },
+  {
+    name: "Two Boxes",
+    site: "https://www.twoboxes.com/",
+    album: "images/albums/album-two-boxes.png",
+    description: "Reverse logistics software",
+    liquidEvent: false
+  },
+  {
+    name: "Typeface",
+    site: "https://www.typeface.ai/",
+    album: "images/albums/album-typeface.png",
+    description: "AI-powered design tools",
+    liquidEvent: false
+  },
+  {
+    name: "Xeno",
+    site: "https://getxeno.com/",
+    album: "images/albums/album-xeno.png",
+    description: "India’s Retail CRM",
+    liquidEvent: false
   }
-]
+];
